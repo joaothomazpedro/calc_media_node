7f18d60712705a6f55561e48e018a2e3494edebb
João Pedro Thomaz <jojopepedro@hotmail.com>
2024-07-22 12:48:10 -0300

    Estrutura do Código Pronta

--- index2.js
+++ index2.js
@@ -1,20 +1,50 @@
-const prompt = require("prompt-sync")();
+/* Requisito de biblioteca */
+const prompt = require("prompt-sync")(); 
 
-const alunos = {};
-const materias = ['Português', 'Matemática', 'História', 'Geografia', 'Ciências']
+/* Inicialização de variáveis */
+const aluno = [];
+const materia = [];
 
+/* Começo da interface */
 while(true){
-    const quest = prompt("Deseja cadastrar um aluno? [Y/N]: ");
+    const quest = prompt("Deseja cadastrar um aluno? [Y/N]: "); /* Prompt Inicial */
     
     if(quest == "y" || quest == "Y"){        
-        
+
         const nome_aluno_cad = prompt("Cadastre um aluno: ");
 
-        alunos.push({"Nome": nome_aluno_cad, "Matérias" : materias});
+        /* Cadastro do aluno com verificação de erro: Nome vazio ou Inválido */
+        if(nome_aluno_cad != NaN || nome_aluno_cad != ""){
+            
+            aluno.push(nome_aluno_cad)
 
-        console.log(alunos);
+            const materia_count = prompt("Deseja cadastrar o aluno em quantas matérias (Mínimo de três matérias): ")
+
+            if(materia_count >= 3 && materia_count < 11){
+                for (let i = 0; i < materia_count; i++) {
+            
+                    const materia_cad = prompt("Cadastre uma matéria: ")
+
+                    materia.push(materia_cad)                    
+                    
+                }
+                            
+            }
+            /* Tratamento de erro: Quantidade de Matérias */
+            else{
+                console.log("Escolha um número válido");                
+            }
+
+        }
+        /* Tratamento de erro: Nome Inválido */
+        else{
+            console.log("Digite um nome válido")
+            return
+        }            
     }
-    else{
+
+    /* Caso nenhum aluno seja cadastrado o código encerra */
+    else{        
         console.log("Ok, até outro dia");
         break
     }
